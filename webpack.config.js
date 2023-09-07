4248f0f0b69a2a46ac359c352e978e3a6802e699
Zipp300 <notepad300@yandex.com>
2023-09-07 07:58:19 -0500

    currentTask = "dev" OR "build"

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,57 @@
-const path = require('path')
-
-// const postCSSPlugins = [
-// require('postcss-import'),
-// require('postcss-simple-vars'),
-// require('postcss-nested'),
-// require('autoprefixer')
-// ]
-// const postcssSimpleVars =  require('postcss-simple-vars')
+const currentTask = process.env.npm_lifecycle_event
+const path = require("path")
 
 const postCSSPlugins = [
-  'postcss-import',
-  'postcss-mixins',
-  'postcss-simple-vars',
-  'postcss-nested',
-  'autoprefixer'
-  ]
+  "postcss-import",
+  "postcss-mixins",
+  "postcss-simple-vars",
+  "postcss-nested",
+  "autoprefixer",
+]
 
-
-module.exports = {
-  entry: './app/assets/scripts/App.js',
-  output: {
-    filename: 'bundled.js',
-    path: path.resolve(__dirname, 'app')
-  },
-  devServer: {
-    
-    //contentBase: path.join(__dirname, 'app'),
-    static: {directory: path.join(__dirname, "app")},
-    hot: true,
-    port: 3000,
-    historyApiFallback: { index: "index.html" }
-  },
-  mode: 'development',
+let config = {
+  entry: "./app/assets/scripts/App.js",
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader', {loader: 'postcss-loader', options: {
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
               postcssOptions: {
-                plugins: postCSSPlugins
-              }
-            }
-          }
-        ]
-      }
-    ]
-  }
+                plugins: postCSSPlugins,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
 }
+
+if (currentTask == "dev") {
+  config.output = {
+    filename: "bundled.js",
+    path: path.resolve(__dirname, "app"),
+  }
+  config.devServer = {
+    static: { directory: path.join(__dirname, "app") },
+    hot: true,
+    port: 3000,
+    // host: "0.0.0.0",
+    historyApiFallback: { index: "index.html" },
+  }
+  config.mode = "development"
+}
+if (currentTask == "build") {
+  config.output = {
+    filename: "bundled.js",
+    path: path.resolve(__dirname, "dist"),
+  }
+  config.mode = "production"
+}
+
+module.exports = config
