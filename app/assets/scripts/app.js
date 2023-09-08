75c2e306b2b8b329cd67ea45b993eff3ba74df84
Zipp300 <notepad300@yandex.com>
2023-09-08 18:49:07 -0500

    splitChunks / MiniCssExtractPlugin / cssnano

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,5 +1,7 @@
 import "../styles/styles.css"
 
+alert("Cambiando el archivo!!!")
+
 if (module.hot) {
   module.hot.accept()
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const currentTask = process.env.npm_lifecycle_event
 const path = require("path")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
 const postCSSPlugins = [
   "postcss-import",
@@ -9,30 +11,30 @@ const postCSSPlugins = [
   "autoprefixer",
 ]
 
+let cssConfig = {
+  test: /\.css$/i,
+  use: [
+    "css-loader",
+    {
+      loader: "postcss-loader",
+      options: {
+        postcssOptions: {
+          plugins: postCSSPlugins,
+        },
+      },
+    },
+  ],
+}
+
 let config = {
   entry: "./app/assets/scripts/App.js",
   module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: [
-          "style-loader",
-          "css-loader",
-          {
-            loader: "postcss-loader",
-            options: {
-              postcssOptions: {
-                plugins: postCSSPlugins,
-              },
-            },
-          },
-        ],
-      },
-    ],
+    rules: [cssConfig],
   },
 }
 
 if (currentTask == "dev") {
+  cssConfig.use.unshift("style-loader")
   config.output = {
     filename: "bundled.js",
     path: path.resolve(__dirname, "app"),
@@ -47,11 +49,22 @@ if (currentTask == "dev") {
   config.mode = "development"
 }
 if (currentTask == "build") {
+  cssConfig.use.unshift(MiniCssExtractPlugin.loader)
+  // * El CSS resultante de los PostCSS plugIns se comprime al final
+  postCSSPlugins.push("cssnano")
   config.output = {
-    filename: "bundled.js",
+    filename: "[name].[chunkhash].js",
+    chunkFilename: "[name].[chunkhash].js",
     path: path.resolve(__dirname, "dist"),
   }
   config.mode = "production"
+  config.optimization = {
+    splitChunks: { chunks: "all" },
+  }
+  config.plugins = [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({ filename: "styles.[chunkhash].css" }),
+  ]
 }
 
 module.exports = config
